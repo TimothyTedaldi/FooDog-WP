e5470ff24b4257d76624bf53c0f898d4cd441940
Steeve1301 <steevehenrard5@gmail.com>
2020-05-29 01:24:38 +0200

    Update

---
+++ wp-content/themes/FooDog/functions.php
@@ -0,0 +1,57 @@
+<?php
+
+function register_my_menu(){
+   register_nav_menu('main', "Menu principal");
+}
+
+add_action('after_setup_theme', 'register_my_menu');
+
+
+function wpbootstrap_styles_scripts(){
+    wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', array('jquery'), 1, true);
+    wp_enqueue_script('boostrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', array('jquery', 'popper'), 1, true);
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'wpbootstrap_styles_scripts');
+
+
+function mytheme_post_thumbnails() {
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'mytheme_post_thumbnails' );
+
+function register_my_sidebars(){
+    register_sidebar(
+        array(
+            'name' => "Sidebar principale",
+            'id' => 'main-sidebar',
+            'description' => "La sidebar principale",
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>'
+        )
+    );
+   
+    register_sidebar(
+        array(
+            'name' => "Sidebar du footer",
+            'id' => 'footer-sidebar',
+            'description' => "La sidebar principale",
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>'
+        )
+    );
+ }
+ add_action('widgets_init', 'register_my_sidebars');
+
+ add_image_size('slider-thumb', 542,341, true );
+ add_image_size('pop-thumb',542, 340, true );
+ add_image_size('lens-thumb',542, 410, true );
+ add_image_size('photo-thumb',800, 600, true );
+
+?>
